8d0003eb50b2d98ecda09c203bea9c82e8424821
Teamer Tibebu <teamertibebu@gmail.com>
2026-06-07 22:45:47 -0400

    understanding data structures: lists, dictionaries, tuples

--- Introduction to Python/Data structures/Add Items from lists/lists_to_dict.py
+++ Introduction to Python/Data structures/Add Items from lists/lists_to_dict.py
@@ -4,6 +4,10 @@ list_of_values = [100, 200, 300]
 my_dict = {}
 for x in range(len(list_of_keys)):
     # TODO: populate the dictionary with key:value pairs from the lists
+    key = list_of_keys[x]
+    val = list_of_values[x]
+
+    my_dict[key] = val
 
 print(my_dict.items())
 
